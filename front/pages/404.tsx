90b19f354301d9e8da76dcc007902d7029618d78
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-01-10 09:07:02 -0300

    'build' - 404 page

--- front/pages/404.tsx
+++ front/pages/404.tsx
@@ -1,10 +1,18 @@
-
+import Footer from "../components/footer/Footer";
+import BG from '../public/images/404/NotFoundBG.png'
+import Slowpoke from '../public/images/404/NotFoundIcon.png'
 
 export default function NotFoundPage() {
     return (
-      <>
-        <h1>Login</h1>
-      </>
+     <div className="h-screen flex flex-col">
+        <main className="grow notfound-main">
+            <p className="text-xl lg:text-4xl font-medium">Desculpe, não entendi o que você esta procurando...</p>
+            <p className="text-base lg:text-2xl italic">A página que você procura não existe ou esta indisponivel</p>
+            <img src={Slowpoke.src} alt="Slowpoke-not-found" />
+            <h1 className="font-bold uppercase text-6xl lg:text-7xl">Erro 404</h1>
+        </main>
+        <Footer></Footer>
+     </div>
     );
   }
   
