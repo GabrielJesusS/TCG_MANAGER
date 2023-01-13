dd566b8217efc6740296be8a7cba094917853ba7
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-01-12 23:18:38 -0300

    'ci' deck item improve

--- front/components/deckItem/DeckItem.tsx
+++ front/components/deckItem/DeckItem.tsx
@@ -1,11 +1,31 @@
+
 export interface DeckItemProps{
-    sampleTextProp: string;
+    deckID: number;
+    deckTitle: string
+    deckAuthor: string
+    deckCover: string
+    deckDesc: string
+    deckEffic: number
+    deckDif: number
+    deckWr:number
 }
 
-const DeckItem: React.FC<DeckItemProps> = ({sampleTextProp})=>{
+const DeckItem: React.FC<DeckItemProps> = ({deckAuthor,deckCover,deckDesc,deckDif,deckEffic,deckTitle,deckWr})=>{
     return (
-        <div>
-            
+        <div className="deck-container">
+            <div className="deck-cover">
+                <img className="deck-cover-img" src={deckCover} alt=""/>
+            </div>
+            <div className="deck-infos">
+                <a href="#" className="deck-title">{deckTitle}</a>
+                <a href="#" className="deck-author">{deckAuthor}</a>
+                <p className="deck-desc">{deckDesc}</p>
+                <div className="deck-data">
+                    <p className="deck-effic">Efficiência: {deckEffic.toFixed(1)}</p>
+                    <p className="deck-diffic">Dificuldade: {deckDif.toFixed(1)}</p>
+                    <p className="deck-wr">Taxa de vitória: {deckWr}%</p>
+                </div>
+            </div>
         </div>
     )
 }
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -4,11 +4,26 @@ import Input from "../components/input/TextInput";
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/footer/Footer";
 import ArticleList from "../components/articlesList/ArticlesList";
+import DeckItem,{DeckItemProps} from "../components/deckItem/DeckItem";
+import Cover from '../public/images/card-mock.png'
+
+
+const deckItemInfo:DeckItemProps = {
+  deckID: 1,
+  deckAuthor: "Gabriel Jesus",
+  deckCover: Cover.src,
+  deckDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ullam distinctio eius facilis laboriosam, maxime temporibus.",
+  deckDif: 5,
+  deckEffic: 5,
+  deckTitle: 'Blastoise doido',
+  deckWr: 100
+}
+
 
 export default function Home() {
   return (
     <>
-     
+     <DeckItem {...deckItemInfo}></DeckItem>
     </>
   );
 }
