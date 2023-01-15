ee41a0cb4b09eaaf66917a9a6deb4f1fb23deb4f
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-01-14 23:51:11 -0300

    'build' - login page

--- front/components/input/TextInput.tsx
+++ front/components/input/TextInput.tsx
@@ -5,13 +5,14 @@ export interface InputProps{
    variant: 'primary' | 'error'
    className?: string;
    disabled?: boolean;
+   type?: 'text' | 'password' | 'email' | 'search'
 }
 
-const TextInput: React.FC<InputProps> = ({icon, placeholder, disabled})=>{
+const TextInput: React.FC<InputProps> = ({icon, placeholder, disabled, className, type})=>{
     return (
-        <span className={`dft-input ${disabled? 'bg-system-gray-100' : null}`}>
+        <span className={`dft-input ${className} ${disabled? 'bg-system-gray-100' : null}`}>
             <span className="input-icon">{icon? <span>{icon}</span>: null}</span>
-            <input disabled={disabled} className="input-box" placeholder={placeholder} type="text"/>
+            <input disabled={disabled} className="input-box" placeholder={placeholder} type={type || 'text'}/>
         </span>
     )
 }
--- front/pages/login.tsx
+++ front/pages/login.tsx
@@ -1,11 +1,39 @@
-import Button from "../components/button/Button";
-import {MdBattery90} from 'react-icons/md'
-import Input from "../components/input/TextInput";
+import Button from '../components/button/Button';
+import { MdBattery90 } from 'react-icons/md';
+import Input from '../components/input/TextInput';
+import Logo from '../public/icons/logo-svg.svg';
+import Footer from '../components/footer/Footer';
+import TextInput from '../components/input/TextInput';
 
 export default function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <div className="flex flex-col h-screen">
+        <main className="w-full h-full justify-center flex bg-blue-500">
+          <div className='flex w-full justify-around items-center max-w-7xl'>
+            <div className='flex'>
+              <form action="post" className='flex flex-col items-center py-8 px-12 text-xl text-system-gray-900 bg-white rounded-2xl'>
+                <Logo height={112} width={248}/>
+                <h1 className='font-bold text-2xl my-16'>Autenticação</h1>
+                <label>
+                  Email:
+                  <TextInput type='email' className='max-w-[300px] text-base mb-8' variant="primary" placeholder="Email..."></TextInput>
+                </label>
+                <label>
+                  Senha:
+                  <TextInput type='password' className='max-w-[300px] text-base mb-8' variant="primary" placeholder="Senha..."></TextInput>
+                </label>
+                <p className='text-base mb-8'>Não possui registro? Registre-se <a className='hover:text-secondary-main' href="/register">aqui!</a></p>
+                <Button variant='primary'>Realizar login</Button>
+              </form>
+            </div>
+            <div>
+              <img width={320} src="https://images.pokemontcg.io/pl3/1.png" alt="" />
+            </div>
+          </div>
+        </main>
+        <Footer></Footer>
+      </div>
     </>
   );
 }
